32a5ce61891842d9a785f87010a5892e9b423acc
Just_YOLO <nascarf16@gmail.com>
2023-01-14 23:04:46 +0900

    Day_12 end

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-float_value1 = 0.1234
-float_value2 = 0.5678
-int_value = 10
-
-print("%.3f" % float_value1)
-print("%.3f" % float_value2)
-print("%.3f" % int_value)
+testlist = [True] * 4
+print(testlist)
+print(testlist[0])
+print(testlist[3])
